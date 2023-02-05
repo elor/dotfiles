afa9239677875204baaacc2ea6442833400d844e
Erik E. Lorenz <erik@tuvero.de>
2023-02-05 16:13:23 +0100

    Remove haskell-tools.nvim, which didn't work for me

--- private_dot_config/nvim/lua/elor/packer.lua
+++ private_dot_config/nvim/lua/elor/packer.lua
@@ -24,7 +24,7 @@ require('packer').startup(function(use)
 
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
-      --
+
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
 
@@ -38,6 +38,7 @@ require('packer').startup(function(use)
 
       -- Snippets
       { 'L3MON4D3/LuaSnip' },
+
       -- Snippet Collection (Optional)
       { 'rafamadriz/friendly-snippets' },
     }
@@ -87,15 +88,6 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-
-  use {
-    'mrcjkb/haskell-tools.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim', -- optional
-    },
-    branch = '1.x.x', -- recommended
-  }
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
