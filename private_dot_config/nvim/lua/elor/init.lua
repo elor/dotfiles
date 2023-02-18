8b9a18778e3cae4b0223d37ce4a70cd69aa73416
Erik E. Lorenz <erik@tuvero.de>
2023-02-18 14:39:17 +0100

    nvim telescope: close buffers with C-d

--- private_dot_config/nvim/lua/elor/init.lua
+++ private_dot_config/nvim/lua/elor/init.lua
@@ -44,7 +44,10 @@ require('telescope').setup {
     mappings = {
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
+      },
+      n = {
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
       },
     },
     layout_config = {
