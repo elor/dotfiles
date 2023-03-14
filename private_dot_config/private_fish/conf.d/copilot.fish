59e1581a2e0690b3b0793b3479688e0a690d2d14
Erik E. Lorenz <erik@tuvero.de>
2023-03-14 08:59:17 +0100

    Copilot CLI: Simplify fish

--- private_dot_config/private_fish/conf.d/copilot.fish
+++ private_dot_config/private_fish/conf.d/copilot.fish
@@ -1,18 +1,10 @@
-function copilot_what-the-shell
+function cop # github copilot
   set TMPFILE (mktemp);
   trap "rm -f $TMPFILE" EXIT;
 
   if /opt/homebrew/bin/github-copilot-cli what-the-shell "$argv" --shellout $TMPFILE
-    if [ -e "$TMPFILE" ]
-      let FIXED_CMD "$(cat $TMPFILE)";
-      
-      eval "$FIXED_CMD"
-    else
-      echo "Apologies! Extracting command failed"
-    end
+    source $TMPFILE
   else
     return 1
   end
 end
-
-alias 'cop' 'copilot';
