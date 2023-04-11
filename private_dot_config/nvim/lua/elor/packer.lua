e7b9468210a8fded4b9eb0d90e4a7a60db515fa8
Erik E. Lorenz <erik@tuvero.de>
2023-04-11 21:13:52 +0200

    nvim: autoformat packer.lua

--- private_dot_config/nvim/lua/elor/packer.lua
+++ private_dot_config/nvim/lua/elor/packer.lua
@@ -80,14 +80,14 @@ require('packer').startup(function(use)
   use 'ryanoasis/vim-devicons'
 
   -- Git related plugins
-  use 'lewis6991/gitsigns.nvim' -- Git signs at each line
-  use 'kdheepak/lazygit.nvim' -- interactive git tool; it's amazing
+  use 'lewis6991/gitsigns.nvim'             -- Git signs at each line
+  use 'kdheepak/lazygit.nvim'               -- interactive git tool; it's amazing
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  use 'navarasu/onedark.nvim'               -- Theme inspired by Atom
+  use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use 'numToStr/Comment.nvim'               -- "gc" to comment visual regions/lines
+  use 'tpope/vim-sleuth'                    -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
