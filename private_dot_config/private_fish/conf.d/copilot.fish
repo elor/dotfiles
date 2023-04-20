4cc612130c79889545d6b19df442dff46ff510a5
Erik E. Lorenz <erik@tuvero.de>
2023-04-20 17:57:39 +0200

    copilot-cli: add proposed command to fish_history

--- private_dot_config/private_fish/conf.d/copilot.fish
+++ private_dot_config/private_fish/conf.d/copilot.fish
@@ -1,8 +1,11 @@
-  set TMPFILE (mktemp);
-  trap "rm -f $TMPFILE" EXIT;
 function !! # github copilot
+  set TMPFILE (mktemp)
+  trap "rm -f $TMPFILE" EXIT
 
   if /opt/homebrew/bin/github-copilot-cli what-the-shell "$argv" --shellout $TMPFILE
+    echo "- cmd: $(cat $TMPFILE)" >> "$__fish_user_data_dir/fish_history"
+    echo "  when: $(date +%s)" >> "$__fish_user_data_dir/fish_history"
+    history merge
     bash $TMPFILE
   else
     return 1
