0288a446e8ea95df451da83f6d9b7af167e8bc93
Erik E. Lorenz <erik@tuvero.de>
2023-04-30 11:59:28 +0200

    fish copilot: indent before writing to history, as not to break it

--- private_dot_config/private_fish/conf.d/copilot.fish
+++ private_dot_config/private_fish/conf.d/copilot.fish
@@ -3,7 +3,7 @@ function !! # github copilot
   trap "rm -f $TMPFILE" EXIT
 
   if /opt/homebrew/bin/github-copilot-cli what-the-shell "$argv" --shellout $TMPFILE
-    echo "- cmd: $(cat $TMPFILE)" >> "$__fish_user_data_dir/fish_history"
+    echo "- cmd: $(sed 's/^([^ ].|.[^ ])/  &/' $TMPFILE)" >> "$__fish_user_data_dir/fish_history"
     echo "  when: $(date +%s)" >> "$__fish_user_data_dir/fish_history"
     history merge
     bash $TMPFILE
