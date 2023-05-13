243460ad1507cc9eaa2ebfbabc4329ebc948821c
Erik E. Lorenz <erik@tuvero.de>
2023-05-13 07:32:15 +0200

    nvim dap: remove unused pwa debugging config

--- private_dot_config/nvim/lua/elor/init.lua
+++ private_dot_config/nvim/lua/elor/init.lua
@@ -116,28 +116,6 @@ dap.configurations.javascript = {
     program = "${file}",
     cwd = "${workspaceFolder}",
   },
-  -- {
-  --   type = "pwa-node",
-  --   request = "attach",
-  --   name = "Attach",
-  --   processId = require("dap.utils").pick_process,
-  --   cwd = "${workspaceFolder}",
-  -- },
-  -- {
-  --   type = "pwa-node",
-  --   request = "launch",
-  --   name = "Debug Jest Tests",
-  --   -- trace = true, -- include debugger info
-  --   runtimeExecutable = "node",
-  --   runtimeArgs = {
-  --     "./node_modules/jest/bin/jest.js",
-  --     "--runInBand",
-  --   },
-  --   rootPath = "${workspaceFolder}",
-  --   cwd = "${workspaceFolder}",
-  --   console = "integratedTerminal",
-  --   internalConsoleOptions = "neverOpen",
-  -- },
 }
 
 -- [[ Configure Telescope ]]
