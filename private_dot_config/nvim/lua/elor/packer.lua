d6f5864e1c26458d2e505fb698324fab3866e646
Erik E. Lorenz <erik@tuvero.de>
2024-01-17 10:28:00 +0100

    nvim: update neo-tree to v3.x

--- private_dot_config/nvim/lua/elor/packer.lua
+++ private_dot_config/nvim/lua/elor/packer.lua
@@ -169,7 +169,7 @@ require('packer').startup(function(use)
   -- neo-tree: file explorer
   use {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
+    branch = "v3.x",
     requires = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
