7c3165b310ba709d58a6efd210a670e13ab79ebb
Erik E. Lorenz <erik.lorenz@uni-leipzig.de>
2025-10-02 11:40:27 +0200

    nvim: add <leader>C hotkey to CodeCompanion

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map <leader>cc to :CodeCompanionChat
+vim.keymap.set(
+  "n",
+  "<leader>C",
+  ":CodeCompanionChat @{full_stack_dev} #{buffer} #{lsp}<CR>",
+  { desc = "CodeCompanion Chat" }
+)
