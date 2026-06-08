4f4504a3f821eaaf8ebe336ab768743c32c0201d
Erik E. Lorenz <erik.lorenz@uni-leipzig.de>
2026-06-08 13:50:29 +0200

    fish: fix TERM env for remote tmux sessions via remux

--- private_dot_config/private_fish/functions/remux.fish
+++ private_dot_config/private_fish/functions/remux.fish
@@ -1,3 +1,3 @@
 function remux
-    ssh $argv[1] -t 'tmux new-window -s erik || tmux new-session -t erik'
+    ssh $argv[1] -t 'TERM=xterm-256color tmux new-window -s erik || xterm-256color tmux new-session -t erik'
 end
