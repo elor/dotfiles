cf7dd2d47ea0e90d4f1fbdf641923b0742a0ed0f
Erik E. Lorenz <erik.lorenz@uni-leipzig.de>
2026-06-18 09:03:53 +0200

    feat: add events-to-obsidian script for daily calendar sync

---
+++ private_dot_local/bin/executable_events-to-obsidian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -u
+
+# Get today's date in ISO format
+TODAY=$(date +"%Y-%m-%d")
+VAULT=~/Documents/Speicherwolke/Notes/
+
+# Get the calendar events for today using icalBuddy
+{
+  obsidian-macos-calendar-bridge
+} | sed -i '' '/^#* Meetings$/r /dev/stdin' $VAULT/Daily/"$TODAY.md"
+
+echo "wrote events to $VAULT/Daily/$TODAY.md"
